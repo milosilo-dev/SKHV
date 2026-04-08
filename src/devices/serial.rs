41623c66a1568202f61bcb0081c81833c4a255f6
milosilo-dev <lazerache@gmail.com>
2026-04-08 18:42:28 +0100

    Added two com ports

--- src/devices/serial.rs
+++ src/devices/serial.rs
@@ -1,15 +1,56 @@
+use std::{collections::VecDeque, io::{self, Write}};
+
 use crate::device_maps::io::IODevice;
 
-pub struct Serial{}
+pub struct Serial{
+    data: VecDeque<u8>,
+    new_data: bool,
+}
 
-impl IODevice for Serial {
-    fn input(&mut self, _port: u16, length: usize) -> Vec<u8> {
-        vec![0; length]
+impl Serial {
+    pub fn new() -> Self {
+        Self{
+            data: vec![].into(),
+            new_data: false,
+        }
     }
 
-    fn output(&mut self, _port: u16, data: &[u8]) {
-        for i in 0..data.len(){
-            print!("{}", data[i] as char);
+    pub fn set_data(&mut self, new_data: Vec<u8>) {
+        self.data = new_data.into();
+        self.new_data = true;
+    }
+}
+
+impl IODevice for Serial {
+    fn input(&mut self, port: u16, length: usize) -> Vec<u8> {
+        match port{
+            0 => {
+                let mut out = vec![0; length];
+                for i in 0..length {
+                    let next_byte = self.data.pop_front();
+                    if next_byte.is_some() {
+                        out[i] = next_byte.unwrap();
+                    }
+                }
+                out
+            }
+            5 => {
+                let status = if self.new_data {0x01} else {0x20};
+                vec![status; length]
+            }
+            _ => {vec![0; length]}
+        }
+    }
+
+    fn output(&mut self, port: u16, data: &[u8]) {
+        match port{
+            0 => {
+                for i in 0..data.len(){
+                    print!("{}", data[i] as char);
+                }
+                io::stdout().flush().unwrap();
+            }
+            _ => {}
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@ fn main() {
     let init_mem_image = fs::read("guest/firmware.bin").unwrap();
     let mut vm = VirtualMachine::new(Vec::from(init_mem_image));
 
-    let serial_device = Box::new(Serial{});
-    vm.register_io_device(IODeviceRegion::new(0x3f8..=0x3f8, serial_device));
+    let com1 = Box::new(Serial::new());
+    vm.register_io_device(IODeviceRegion::new(0x3f8..=0x3ff, com1));
+
+    let com2 = Box::new(Serial::new());
+    vm.register_io_device(IODeviceRegion::new(0x2f8..=0x2ff, com2));
 
     loop {
         let ret = vm.run();
